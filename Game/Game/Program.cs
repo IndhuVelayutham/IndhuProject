e771bf68e47b9a58a33a3fa2fed0140b90239344
indhumathi-velayutham <mathi.17191@gmail.com>
2017-09-25 17:37:30 +1200

    Simple Box open Game
    You are given 40 boxes with a characters inside each box. The each are same or different but you have no idea what they are. You pick one box to open; read the inside; and then guess  the other box , should be same. You win if you guess correctly, and lose otherwise. Is there anyway that you can win the game with more than 50% chances no matter what the two numbers are?

---
+++ Game/Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    class Program
+    {
+
+        static List<char> box = new List<char> { 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f', 'g', 'g', 'h', 'h', 'i', 'i', 'j', 'j' };
+        static void Main(string[] args)
+        {
+
+            int userChance = 40, prevIndex, currIndex, indexer = 1;
+            prevIndex = currIndex = 0;
+            bool win = false;
+            //User Chances
+            while (userChance > 0)
+            {
+                prevIndex = currIndex;
+                Console.WriteLine("Please select a box from below:");
+                indexer = 1;
+                //display available boxes to User
+                foreach (char item in box)
+                {
+                    Console.WriteLine("Box: {0}", indexer);
+                    indexer++;
+                }
+
+                //User Selected box
+                currIndex = Convert.ToInt32(Console.ReadLine());
+                if (prevIndex > 0)
+                {
+                    if (box[prevIndex - 1].Equals(box[currIndex - 1]))
+                    {
+                        box.RemoveAt(currIndex - 1);
+                        box.RemoveAt(prevIndex - 1);
+                        prevIndex = currIndex = 0;
+                    }
+                }
+
+                //User chance gets Reduced
+                userChance--;
+
+                //Deciding game status
+                if (box.Count == 0)
+                {
+                    win = true;
+                    break;
+                }
+
+            }
+            // display game status to user
+            if (win)
+                Console.WriteLine("User wins");
+            else
+                Console.WriteLine("User Lost");
+            Console.ReadKey();
+        }
+    }
+}
